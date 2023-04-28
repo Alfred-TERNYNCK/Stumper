57ee801a6357160d516240ae8e95bfba597e515b
alfred <ternnckalfred@gmail.com>
2023-04-28 12:44:32 +0200

    [FIX] coding style and norm for makefile

--- S5/src/main.c
+++ S5/src/main.c
@@ -11,12 +11,15 @@
 
 int initialiser_mots(int argc, char ** argv, struct GameInfo* game_info)
 {
+    if (argc != 2) {
+        printf("Erreur: Nombre d'arguments invalide\n");
+        return ERROR;
+    }
     FILE* fichier = fopen(argv[1], "r");
     if (fichier == NULL) {
         printf("Erreur: Impossible d'ouvrir le fichier %s\n", argv[1]);
         return ERROR;
     }
-
     game_info->num_words = 0;
     while (fgets(game_info->words[game_info->num_words],
     MAX_WORD_LENGTH, fichier)) {
@@ -36,8 +39,9 @@ void initialiser_lettres_trouvees(struct GameInfo* game_info)
 
 void choisir_mot(struct GameInfo* game_info)
 {
+    int index;
     srand(time(NULL));
-    int index = rand() % game_info->num_words;
+    index = rand() % game_info->num_words;
     game_info->word = game_info->words[index];
     game_info->word_length = strlen(game_info->word);
 }
@@ -45,7 +49,6 @@ void choisir_mot(struct GameInfo* game_info)
 int main(int argc, char* argv[])
 {
     struct GameInfo game_info;
-    char input;
 
     if (initialiser_mots(argc, argv, &game_info) == ERROR)
         return ERROR;
